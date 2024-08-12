082d0b4367218e52785f18bdbcb55ed3538f6b0d
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-12 23:18:55 +0300

    feat: update config & jwt expiresIn

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,14 @@ import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-import { JWT_SECRET_KEY } from './constants/config';
+import { JWT_EXPIRATION_TIME, JWT_SECRET_KEY } from './constants/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: JWT_SECRET_KEY,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: JWT_EXPIRATION_TIME },
     }),
   ],
   providers: [AuthService, JwtStrategy],
--- src/auth/constants/config.ts
+++ src/auth/constants/config.ts
@@ -1,2 +1,3 @@
 export const JWT_SECRET_KEY =
   process.env.JWT_SECRET_KEY || 'default_secret_key';
+export const JWT_EXPIRATION_TIME = '24h';
