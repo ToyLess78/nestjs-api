65fa5ebb2e16456c5459522db85a8cf7ea50c238
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-14 11:53:09 +0300

    fix: update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { Trip } from './trips/entities/trip.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT, 10),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
       entities: [User, Trip],
       synchronize: true,
       ssl: {
