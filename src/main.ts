ddd13cf615ae082fc09dfea8060d217116d792ac
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-14 14:39:38 +0300

    fix: add logs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   console.log('DATABASE_URL:', process.env.DATABASE_URL);
+  console.log('DATABASE_PUBLIC_URL:', process.env.DATABASE_PUBLIC_URL);
+  console.log('JWT_SECRET_KEY:', process.env.JWT_SECRET_KEY);
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
