4db7e9b04dd693c3480528bc3b019698409a51b3
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-14 16:29:24 +0300

    fix: update main

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('DATABASE_URL:', process.env.DATABASE_URL);
-  console.log('DATABASE_PUBLIC_URL:', process.env.DATABASE_PUBLIC_URL);
-  console.log('JWT_SECRET_KEY:', process.env.JWT_SECRET_KEY);
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
