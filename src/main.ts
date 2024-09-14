2a523afa8e1a03dca7012ad28e2e89179f203569
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-09-14 11:54:55 +0300

    feat: add main swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,18 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Travel API')
+    .setDescription('API for booking trips')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
+
+  const logger = new Logger('Bootstrap');
+  logger.log('Swagger is running on: http://localhost:3000/documentation/');
 }
 bootstrap();
